3bb1fad49e89bc58b3e3e6dbfb1c12a6d5c8559a
5681jin <5681jin@naver.com>
2018-10-04 16:09:29 +0900

    전시회 프로젝트 생성

---
+++ 굴러라! BOX/Assets/Script/boxmove.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxmove : MonoBehaviour
+{
+
+    public float JumpPower = 15;
+    int KeyInputCheck = 0;
+    int RightLeftDistinction = 0;//좌우판별
+    int BoxRotation = 1;//박스의 회전상태를 나타냄, 1일때 정지
+    public bool BoxOnGround = true;//상자가 땅에 있는지 떠있는지 판별함.
+
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+// Update is called once per frame
+    void Update ()
+    {
+
+        if (BoxOnGround==true&&Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpPower);
+            BoxOnGround = false;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (KeyInputCheck == 0 && BoxRotation == 1)
+            {
+                KeyInputCheck = 1;
+                RightLeftDistinction = 1;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (KeyInputCheck == 0 && BoxRotation == 1)
+            {
+                KeyInputCheck = 1;
+                RightLeftDistinction = -1;
+            }
+        }
+
+        if (KeyInputCheck == 1 && BoxRotation==1)
+        {
+            KeyInputCheck = 0;
+            rb.freezeRotation = true;
+            InvokeRepeating("BoxMove", 0, 0.01f);
+        }
+
+    }
+
+    void BoxMove()
+    {
+        float MoveSpeed = 0.1f;
+        float RotateAngle = 3f;
+        transform.position += new Vector3(RightLeftDistinction * MoveSpeed , 0, 0);//이동
+        //transform.position += new Vector3(-RightLeftDistinction *0.00001f *BoxRotation*(-30*BoxRotation+1)*1.059378145028868f, 0, 0);
+        this.transform.Rotate(new Vector3(0, 0, RightLeftDistinction * -RotateAngle));//회전
+        BoxRotation++;
+        if(BoxRotation>30)
+        {
+            rb.freezeRotation = false;
+            CancelInvoke();
+            BoxRotation = 1;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            BoxOnGround = false;
+        }
+    }//땅에서 벗어났을 때 점프를 못하게함
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer==8)
+        {
+            BoxOnGround = true;
+        }
+    }//땅에 닿았을 때 점프를 다시 할 수 있게 만들어줌
+}
---
+++ 굴러라! BOX/Assets/Script/cameracontroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroll : MonoBehaviour
+{
+    GameObject player;
+	// Use this for initialization
+	void Start ()
+    {
+        this.player = GameObject.Find("box");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 playerPros = this.player.transform.position;
+        transform.position = new Vector3(playerPros.x,transform.position.y, transform.position.z);
+	}
+}
